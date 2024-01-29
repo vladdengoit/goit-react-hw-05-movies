7b3ffce2d07d9f4c0d2fd941db051ff81a03e01c
vladdengoit <114827862+vladdengoit@users.noreply.github.com>
2024-01-29 23:22:05 +0200

    elseChanges

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,3 @@
-import { Route, Routes } from 'react-router-dom';
-import MainMenu from './MainMenu/MainMenu';
-import Home from '../pages/Home/Home';
-import Movies from '../pages/Movies/Movies';
-import SingleMovie from '../pages/SingleMovie/SingleMovie';
-import Reviews from '../pages/Reviews/Reviews';
-import Cast from '../pages/Cast/Cast';
 import AppRoute from './AppRoute';
 
 function App() {
@@ -12,15 +5,6 @@ function App() {
     <>
       <div className="App">
         <AppRoute />
-        {/* <MainMenu />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/movies" element={<Movies />} />
-      <Route path="/movies/:movieId" element={<SingleMovie />} />
-      <Route path="/movies/:movieId/cast" element={<Cast />} />
-      <Route path="/movies/:movieId/reviews" element={<Reviews />} />
-      <Route path="*" element={<Home />} />
-    </Routes> */}
       </div>
     </>
   );
